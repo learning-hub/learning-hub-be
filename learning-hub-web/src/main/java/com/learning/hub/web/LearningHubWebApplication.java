9dc1515e648ee219d4beeedd74ccfcf8b2aebe42
happyboy <947773593@qq.com>
2021-09-07 09:28:28 +0800

    init project

---
+++ learning-hub-web/src/main/java/com/learning/hub/web/LearningHubWebApplication.java
@@ -0,0 +1,15 @@
+package com.learning.hub.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lnh
+ */
+@SpringBootApplication
+public class LearningHubWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningHubWebApplication.class, args);
+    }
+}
